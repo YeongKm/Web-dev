d16a0a864383b616543fd11e498d42dc2a6af7c0
YeongKm <162403635+YeongKm@users.noreply.github.com>
2024-09-20 13:54:21 +0900

    First Test

--- app.js
+++ app.js
@@ -1,11 +1,27 @@
-let startingPokerChips = 100;
-let players = 3;
-let noOfStarterCards = 2;
+let STARTING_POKER_CHIPS = 100;
+let PLAYERS = 3;
+let NO_OF_STARTING_CARDS = 2;
+let gameHasEnded = false;
 
-let PlayerOnePoints = startingPokerChips;
-let PlayerTwoPoints = startingPokerChips;
-let PlayerThreePoints = startingPokerChips;
+let playerOneName = "chloe";
+let playerTwoName = "Jasmine";
+let playerThreeName = "Jen";
 
-PlayerOnePoints -= 50;
-PlayerTwoPoints -= 25;
-PlayerThreePoints += 75;
+console.log(`Welcome! 챔피언십 타이틀은 ${playerOneName},
+    ${playerTwoName}, ${playerThreeName} 중 한 명에게 주어집니다. 각
+    선수는 ${STARTING_POKER_CHIPS} 의 칩을 가지고 시작합니다. 흥미진진한 
+    경기가 될것입니다. 최고의 선수가 승리하길 바랍니다!`);
+
+let playerOnePoints = STARTING_POKER_CHIPS;
+let playerTwoPoints = STARTING_POKER_CHIPS;
+let playerThreePoints = STARTING_POKER_CHIPS;
+
+playerOnePoints -= 50;
+playerTwoPoints -= 25;
+playerThreePoints += 75;
+
+gameHasEnded = ((playerOnePoints + playerTwoPoints) == 0) ||
+                ((playerTwoPoints + playerThreePoints) == 0) ||
+                ((playerOnePoints + playerThreePoints) == 0 );
+
+console.log("게임이 종료되었습니다." , gameHasEnded);
