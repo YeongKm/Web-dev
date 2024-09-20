bd1b2efaa8b592e7de9b48a23a3eabaf3120245e
YeongKm <162403635+YeongKm@users.noreply.github.com>
2024-09-20 13:44:21 +0900

    Second commit

---
+++ app.js
@@ -0,0 +1,11 @@
+let startingPokerChips = 100;
+let players = 3;
+let noOfStarterCards = 2;
+
+let PlayerOnePoints = startingPokerChips;
+let PlayerTwoPoints = startingPokerChips;
+let PlayerThreePoints = startingPokerChips;
+
+PlayerOnePoints -= 50;
+PlayerTwoPoints -= 25;
+PlayerThreePoints += 75;
